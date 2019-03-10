b8d3d34851a0790c6c51ac64a8f49eff8fcc5ff0
Krunal Patel <kppatel727@gmail.com>
2019-03-10 10:50:56 -0700

    added logic for winning

--- script.js
+++ script.js
@@ -22,8 +22,38 @@ function startGame() {
   }
 }
 
+function turn(squareId, player) {
+  document.getElementById(squareId).innerText = player;
+  let gameWon = checkWin(origBoard, player);
+  if (gameWon) {
+    gameOver(gameWon);
+  }
+}
+
 function turnClick(square) {
-  console.log(square.target.id);
+  turn(square.target.id, USER);
+}
+
+function checkWin(board, player) {
+  let plays = board.reduce((a, e, i) => (e === player) ? a.concat(i) : a, []);
+  let gameWon = null;
+  for (let [index, win] of WIN_COMBOS.entries()) {
+    if (win.every(elem => plays.indexOf(elem) > -1)) {
+      gameWon = {index: index, player: player};
+      break;
+    }
+  }
+  return gameWon;
+}
+
+function gameOver(gameWon) {
+  for (let index of WIN_COMBOS[gameWon.index]) {
+    document.getElementById(index).style.backgroundColor =
+    gameWon.player == USER ? "blue" : "red";
+  }
+  for (var i = 0; i < CELLS.length; i++) {
+    CELLS[i].removeEventListener('click', turnClick, false);
+  }
 }
 
 const CELLS = document.querySelectorAll('.cell');
